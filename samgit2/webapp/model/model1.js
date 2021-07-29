872a8c32256227849d04d0176d5c08dc84063154
Ramyasree24 <ramyahemanika@gmail.com>
2021-07-29 08:29:40 +0000

    Added new file in model folder

---
+++ samgit2/webapp/model/model1.js
@@ -0,0 +1,24 @@
+{
+	"LOGINS" [{
+		"UserName": "RamyaSree",
+		"Password": "ramya"
+	}, {
+		"UserName": "Abdul",
+		"Password": "abdul123"
+	}, {
+		"UserName": "Santhosh",
+		"Password": "santhosh"
+	}, {
+		"UserName": "Renuka",
+		"Password": "A_Renuka"
+	}, {
+		"UserName": "Visalakshi",
+		"Password": "visalakshi@M"
+	}, {
+		"UserName": "Madhu",
+		"Password": "madhuP"
+	}, {
+		"UserName": "Rambabu",
+		"Password": "Rambabu_M"
+	}]
+}
